d301c4d31781455669f91552921d9237e830ab37
bhanu velamuri <bhanu@dal.ca>
2018-08-20 00:10:21 -0300

    update

--- src/charandboolean.java
+++ src/charandboolean.java
@@ -3,7 +3,7 @@ public class charandboolean {
         char name = '\u00A9';
         boolean a = false;
         boolean b = true;
-        System.out.println("My name is    " + name);
+        System.out.println("Registerd symbol    " + name);
         System.out.println("b"+ b);
         System.out.println("a"+ a);
 
