221164ab2cf6a0713b804696a29f0f0081e2244c
bhanu velamuri <bhanu@dal.ca>
2018-08-20 00:02:09 -0300

    converting pound to kg

--- src/floatanddouble.java
+++ src/floatanddouble.java
@@ -1,10 +1,15 @@
 public class floatanddouble {
     public static void main(String[] args) {
-        int myIntValue= (int) (5 / 3);
+        int myIntValue= (5 / 3);
         float myFloatValue = 5f/3f;
         double myDoubleValue = 5d/3d;
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("myFloatValue = " + myFloatValue);
         System.out.println("mydoubleValue = " + myDoubleValue);
+        //converting pound to kilograms
+        double p = 200;
+        double kg = p*0.45359237;
+        System.out.println(kg);
+
     }
 }
